24d49c5908d10b1462dcc743178ea823165a1736
Ju-Yeongmin <drodudals1@gmail.com>
2023-04-15 21:46:37 +0900

    first commit

---
+++ src/main/java/com/secondservice/secondservice/SecondServiceController.java
@@ -0,0 +1,33 @@
+package com.secondservice.secondservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/second-service")
+public class SecondServiceController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "welcome to the Second service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        log.info(header);
+        return "hello world in second service";
+    }
+
+
+    @GetMapping("/check")
+    public String check() {
+        return "Hi, there. This is a message from Second service.";
+    }
+
+}
+
+
